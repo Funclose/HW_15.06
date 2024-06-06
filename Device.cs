398b2c3260004cd571c55e862e566fbbcac35b8c
Artem <ar.doroxoff@gmail.com>
2024-06-06 23:23:28 +0300

    com

---
+++ Device.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HW_13_06
+{
+    internal abstract class Device
+    {
+        protected string _name;
+        protected string _sound;
+        protected string _desc;
+
+        public Device(string name, string sound, string desc)
+        {
+            _name = name;
+            _sound = sound;
+            _desc = desc;
+        }
+        public abstract void Sound();
+        public abstract void Show();
+        public abstract void Desc();
+
+    }
+
+    internal class Kettle : Device
+    {
+        
+        public Kettle() : base("kettle", "pshh", "for kettle") { }
+        public override void Sound() 
+        {
+            Console.WriteLine($"my sound {_sound}");
+        }
+        public override void Show() {
+            Console.WriteLine($"my name:{_name}");
+        }
+        public override void Desc()
+        {
+            Console.WriteLine($"this is {_name}, my sound {_sound}");
+        }
+    }
+
+    internal class Microwave:Device
+    {
+        public Microwave() : base("Microwave", "mmmmmmm", "for microwave") { }
+        public override void Sound()
+        {
+            Console.WriteLine($"my sound {_sound}");
+        }
+        public override void Show()
+        {
+            Console.WriteLine($"my name:{_name}");
+        }
+        public override void Desc()
+        {
+            Console.WriteLine($"this is {_name}, my sound {_sound}");
+        }
+    }
+    
+    internal class Steamboat:Device
+    {
+        public Steamboat() : base("boat", "bigPSHHH", "for boat") { }
+        public override void Sound()
+        {
+            Console.WriteLine($"my sound {_sound}");
+        }
+        public override void Show()
+        {
+            Console.WriteLine($"my name:{_name}");
+        }
+        public override void Desc()
+        {
+            Console.WriteLine($"this is {_name}, my sound {_sound}");
+        }
+
+        
+
+    }
+    internal class Car : Device
+    {
+        public Car() : base("car", "vrum", "for car") { }
+        public override void Sound()
+        {
+            Console.WriteLine($"my sound {_sound}");
+        }
+        public override void Show()
+        {
+            Console.WriteLine($"my name:{_name}");
+        }
+        public override void Desc()
+        {
+            Console.WriteLine($"this is {_name}, my sound {_sound}");
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_13_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Device myboat = new Steamboat();
+            myboat.Show();
+            myboat.Sound();
+            myboat.Desc();
+
+            Device mymicrowave = new Microwave();
+            mymicrowave.Show();
+            mymicrowave.Sound();
+            mymicrowave.Desc();
+
+            Device mycar = new Car();
+            mycar.Show();
+            mycar.Sound();
+            mycar.Desc();
+
+
+            Device kettle = new Kettle();
+            kettle.Show();
+            kettle.Sound();
+            kettle.Desc();
+
+
+        }
+    }
+}
